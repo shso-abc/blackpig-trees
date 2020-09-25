e0e445e484d1a2b82abb266a6147a36ee9246756
SHS <bb@abcdeee.com>
2020-09-25 22:42:31 +0900

    test
    MyPage test

---
+++ src/pages/MyPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+// import {useSubheader} from "../../_metronic/layout";
+
+export const MyPage = () => {
+//   const suhbeader = useSubheader();
+//   suhbeader.setTitle("My Custom title");
+
+  return (<>My Page</>);
+    // return <h1>test my page</h1>
+};
